d8cbd1b1fb764ca40d5617f5db935db6a4bf388d
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-23 10:28:27 +0530

    make it better

--- src/components/recentwork.js
+++ src/components/recentwork.js
@@ -23,18 +23,18 @@ return () => {
         <div className="recentwork">
             <h5>checkout our most recent work</h5>
             <div className="recentworkslider">
-  <div className={index===0?'activeslide':index-1===1?'prevslide':'okbro'}><img src='https://themezaa.b-cdn.net/wp-content/uploads/2022/04/litho.jpg'/>  </div>
-<div className={index===1?'activeslide':index-1===1?'prevslide':'okbro'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2019/12/hongo.jpg'/>  </div>
-<div className={index===2?'activeslide':index-1===1?'prevslide':'okbro'}><img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/pofo.jpg'/></div>
-<div className={index===3?'activeslide':index-1===1?'prevslide':'okbro'}><img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-1.jpg"/> </div>
-<div className={index===4?'activeslide':index-1===1?'prevslide':'okbro'} > <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-2.jpg"/> </div>
-<div  className={index===5?'activeslide':index-1===1?'prevslide':'okbro'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-3.jpg'/> </div>
-<div className={index===6?'activeslide':index-1===1?'prevslide':'okbro'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-4.jpg'/> </div>
-<div className={index===7?'activeslide':index-1===1?'prevslide':'okbro'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-5.jpg"/> </div>
-<div className={index===8?'activeslide':index-1===1?'prevslide':'okbro'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-6.jpg"/> </div>
-<div className={index===9?'activeslide':index-1===1?'prevslide':'okbro'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-7.jpg'/> </div>
-<div className={index===10?'activeslide':index-1===1?'prevslide':'okbro'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-8.jpg"/> </div>
-<div className={index===11?'activeslide':index-1===1?'prevslide':'okbro'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2022/04/litho.jpg"/> </div>
+  <div className={index===0?'activeslide':index-1===0?'prevslide':'none'}><img src='https://themezaa.b-cdn.net/wp-content/uploads/2022/04/litho.jpg'/>  </div>
+<div className={index===1?'activeslide':index-1===1?'nextslide':index+1===1?'prevslide':'none'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2019/12/hongo.jpg'/>  </div>
+<div className={index===2?'activeslide':index-1===2?'nextslide':index+1===2?'prevslide':'none'}><img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/pofo.jpg'/></div>
+<div className={index===3?'activeslide':index-1===3?'nextslide':index+1===3?'prevslide':'none'}><img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-1.jpg"/> </div>
+<div className={index===4?'activeslide':index-1===4?'nextslide':index+1===4?'prevslide':'none'} > <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-2.jpg"/> </div>
+<div  className={index===5?'activeslide':index-1===5?'nextslide':index+1===5?'prevslide':'none'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-3.jpg'/> </div>
+<div className={index===6?'activeslide':index-1===6?'nextslide':index+1===6?'prevslide':'none'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-4.jpg'/> </div>
+<div className={index===7?'activeslide':index-1===7?'nextslide':index+1===7?'prevslide':'none'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-5.jpg"/> </div>
+<div className={index===8?'activeslide':index-1===8?'nextslide':index+1===8?'prevslide':'none'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-6.jpg"/> </div>
+<div className={index===9?'activeslide':index-1===9?'nextslide':index+1===9?'prevslide':'none'}> <img src='https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-7.jpg'/> </div>
+<div className={index===10?'activeslide':index-1===10?'nextslide':index+1===10?'prevslide':'none'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2017/02/theme-slider-8.jpg"/> </div>
+<div className={index===11?'activeslide':index-1===11?'nextslide':index+1===11?'prevslide':'none'}> <img src="https://themezaa.b-cdn.net/wp-content/uploads/2022/04/litho.jpg"/> </div>
 </div>
           </div>
         </>
